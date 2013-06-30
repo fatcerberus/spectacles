2590c1284b3ff4d313ff0877260dd1e5935753c5
Bruce E. Pascoe <fatcerberus1@gmail.com>
2013-06-30 01:43:51 -0400

    Polish up opening scene
    This is about as good as it's going to get until I have proper graphics
    and animations to play with.

--- scripts/maps/main.js
+++ scripts/maps/main.js
@@ -1,8 +1,7 @@
 ({
-	enter: function(map, world)
+	openStory: function()
 	{
-		CreatePerson('hero', 'invisible.rss', false);
-		AttachCamera('hero');
+		DetachInput();
 		new Scenario()
 			.fadeTo(CreateColor(0, 0, 0, 255), 0.0)
 			.overrideBGM("BruceTellsHisStory")
@@ -14,7 +13,7 @@
 			.pause(1.0)
 			.talk("Bruce", false, 1.0, "He was.")
 			.pause(2.0)
-			.talk("Bruce", false, 1.0, "The thing that really amazes me, though, even now...")
+			.talk("Bruce", false, 1.0, "Honestly, though? The thing that truly amazes me, even now...")
 			.pause(1.0)
 			.adjustBGMVolume(0.0, 5.0)
 			.pause(1.0)
@@ -28,30 +27,37 @@
 			.talk("Robert", true, 2.0, "There must be some other way, Amanda...")
 			.pause(2.0)  // TODO: Amanda looks into the distance
 			.talk("Amanda", true, 2.0, "Circumstances beyond my control long ago forced my hand. Frankly, I don't see that I have any other choice. At times I wonder if I ever did...")
-			.talk("Robert", true, 2.0, "I just wish you didn't have to leave Lucida to do it.")
+			.talk("Robert", true, 2.0, "I get it, I really do. It's just... well, I just wish you didn't have to leave Lucida to do it.")
 			.pause(1.0)  // TODO: Amanda faces house
 			.talk("Amanda", true, 2.0,
-				"It's not that I don't know how you feel. We all have a lot of memories here... it's home. But I can't just linger around the manor pretending the prophecy will magically go away.",
+				"It's not as though I can't imagine how you feel. We all have a lot of memories here... it's home. But I can't just linger about the manor pretending the prophecy will magically go away.",
 				"Robert, if I don't do something...")
 			.pause(1.0)  // TODO: Amanda uses Flare to set the house ablaze
 			.talk("Amanda", true, 2.0, "Lucida would be destroyed.")
 			.pause(0.5)  // TODO: Robert steps away from burning house
 			.talk("Robert", true, 2.0, "So this is it? You're just going to walk out on all of us?")
-			.talk("Amanda", true, 2.0, "Remember something for me, Robert. Not once have I ever left you to fend for yourself. No matter what's happened, I've always come back.")
+			.talk("Amanda", true, 2.0, "Remember something for me, Robert. Not once have I ever left you to fend for yourself. No matter what's happened, I've always come back. Always.")
 			.talk("Robert", true, 2.0, "Maybe it's nothing. I don't know. But I can't help thinking I'll never see you again. I'm worried, Amanda. More worried than I've ever been about anything. What if something happens? What if you don't return?")
-			.talk("Amanda", true, 2.0, "I'll come back, Robert. That I promise.")
+			.talk("Amanda", true, 2.0, "I'll come back, Robert. I promise.")
 			.fork()
 				.pause(0.5)
 				.pause(1.0)  // TODO: Amanda walks away
 			.end()
 			.pause(2.0)
 			.fork()
-				.fadeTo(CreateColor(0, 0, 0, 255), 2.0)
+				.adjustBGMVolume(0.0, 5.0)
 			.end()
-			.adjustBGMVolume(0.0, 5.0)
-			.synchronize()
+			.fadeTo(CreateColor(0, 0, 0, 255), 2.0)
 			.talk("Bruce", false, 1.0, "...is that it all began with a promise.")
-			.run();
+			.pause(5.0)
+			.run(true);
 		AttachInput('hero');
+	},
+	
+	enter: function(map, world)
+	{
+		CreatePerson('hero', 'invisible.rss', false);
+		AttachCamera('hero');
+		SetDelayScript(0, 'analogue.world.map(\'main.rmp\').openStory();');
 	}
 })
