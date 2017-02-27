40058dd42dadbcb54efb6c4520034a58df76daef
Bruce Pascoe <fatcerberus@icloud.com>
2017-02-27 02:56:27 -0500

    Remove some outdated code
    I'm sure there's more where this came from.  Remnants of past projects
    that either didn't pan out or I just got too lazy to implement.  If I'm
    going to modernize this codebase at all, I'm going to have to do some
    major pruning first.

---
+++ src/session.js
@@ -0,0 +1,36 @@
+/***
+ * Specs Engine v6: Spectacles Saga Game Engine
+  *           Copyright (C) 2012 Power-Command
+***/
+
+RequireScript('FieldMenu.js');
+RequireScript('Party.js');
+
+const Difficulty =
+{
+	Beginner: 1,
+	Standard: 2,
+	Proud:    3,
+	Critical: 4,
+};
+
+class Session
+{
+	static fromFile(fileName)
+	{
+		//TODO: implement me!
+	}
+
+	constructor(difficulty = Difficulty.Standard)
+	{
+		term.print("initializing new game session", "diff lv: " + difficulty);
+
+		this.difficulty = difficulty;
+		this.party = new Party(50);
+		from(Game.initialParty).each(function(characterID) {
+			this.party.add(characterID);
+		}.bind(this));
+		this.battlesSeen = [];
+		this.fieldMenu = new FieldMenu(this);
+	}
+}
