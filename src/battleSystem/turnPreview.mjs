07acf12a6484de409c4f80c47988d5b405d8efe5
Bruce Pascoe <fatcerberus@icloud.com>
2018-03-08 13:17:36 -0500

    Fix alpha fading in TurnPreview
    For now use the miniRT Image shader to support alpha.

--- src/battleSystem/turnPreview.mjs
+++ src/battleSystem/turnPreview.mjs
@@ -21,22 +21,28 @@ class TurnPreview extends Thread
 		let font = Font.Default;
 		let outlineColor = Color.Black.fadeTo(0.25);
 		let surface = new Surface(160, 16);
-		let boxColor = Color.Black.fadeTo(0.66);
-		let borderColor = Color.Black.fadeTo(0.75);
+		let boxColor = Color.Black.fadeTo(0.75);
+		let borderColor = Color.Black.fadeTo(0.85);
 		Prim.drawRectangle(surface, 0, 0, 48, 16, 1.0, borderColor);
 		Prim.drawSolidRectangle(surface, 1, 1, 46, 14, boxColor);
 		font.drawText(surface, 11, 3, "next:", Color.Black);
 		font.drawText(surface, 10, 2, "next:", Color.Silver);
 		Prim.drawRectangle(surface, 48, 0, 112, 16, 1.0, borderColor);
 		Prim.drawSolidRectangle(surface, 49, 1, 110, 14, boxColor);
+		this.shader = new Shader({
+				vertexFile:   '#/shaders/image.vert.glsl',
+				fragmentFile: '#/shaders/image.frag.glsl'
+			});
 		this.transform = new Transform();
-		this.shape = new Shape(ShapeType.TriStrip, surface.toTexture(),
+		let shape = new Shape(ShapeType.TriStrip,
+			surface.toTexture(),
 			new VertexList([
 				{ x: 0,   y: 0,  u: 0.0, v: 1.0 },
 				{ x: 160, y: 0,  u: 1.0, v: 1.0 },
 				{ x: 0,   y: 16, u: 0.0, v: 0.0 },
 				{ x: 160, y: 16, u: 1.0, v: 0.0 },
 			]));
+		this.model = new Model([ shape ], this.shader);
 	}
 
 	dispose()
@@ -47,15 +53,15 @@ class TurnPreview extends Thread
 	ensureEntries(unit)
 	{
 		if (!(unit.tag in this.entries)) {
+			let iconColor = unit.isPartyMember()
+				? Color.DarkSlateBlue
+				: Color.DarkRed;
 			let entry = {
-				color: unit.isPartyMember() ? CreateColor(72, 61, 139, 255) : CreateColor(128, 0, 0, 255),
-				name: unit.name,
-				icon: new BattlerIcon(unit.name, unit.isPartyMember() ? Color.DarkSlateBlue : Color.DarkRed),
+				icon:      new BattlerIcon(unit.name, iconColor, this.shader),
 				turnBoxes: [],
 			};
-			for (let i = 0; i < 8; ++i) {
+			for (let i = 0; i < 8; ++i)
 				entry.turnBoxes[i] = { x: 160, tween: null };
-			}
 			this.entries[unit.tag] = entry;
 		}
 	}
@@ -105,11 +111,12 @@ class TurnPreview extends Thread
 
 	on_render()
 	{
-		let alpha = 255 * (1.0 - this.fadeness);
 		let y = -16 * this.fadeness;
+		let alphaValue = 1.0 - this.fadeness;
 		Surface.Screen.clipTo(0, y, 160, 16);
-		this.transform.identity().translate(0, y);
-		this.shape.draw(Surface.Screen, this.transform);
+		this.shader.setFloatVector('tintColor', [ 1.0, 1.0, 1.0, alphaValue ]);
+		this.model.transform.identity().translate(0, y);
+		this.model.draw(Surface.Screen);
 		for (const id in this.entries) {
 			let entry = this.entries[id];
 			for (let i = 0; i < entry.turnBoxes.length; ++i) {
@@ -123,7 +130,7 @@ class TurnPreview extends Thread
 
 class BattlerIcon
 {
-	constructor(name, color)
+	constructor(name, color, shader)
 	{
 		let font = Font.Default;
 		let outlineColor = Color.Black.fadeTo(0.25);
@@ -133,18 +140,36 @@ class BattlerIcon
 		font.drawText(surface, 5, 3, name[0], Color.Black);
 		font.drawText(surface, 4, 2, name[0], Color.mix(Color.White, color));
 		this.transform = new Transform();
-		this.shape = new Shape(ShapeType.TriStrip, surface.toTexture(),
+		let shape = new Shape(ShapeType.TriStrip,
+			surface.toTexture(),
 			new VertexList([
 				{ x: 0,  y: 0,  u: 0.0, v: 1.0 },
 				{ x: 16, y: 0,  u: 1.0, v: 1.0 },
 				{ x: 0,  y: 16, u: 0.0, v: 0.0 },
 				{ x: 16, y: 16, u: 1.0, v: 0.0 },
 			]));
+		this.model = new Model([ shape ], shader);
 	}
 
 	drawAt(surface, x, y)
 	{
-		this.transform.identity().translate(x, y);
-		this.shape.draw(surface, this.transform);
+		this.model.transform.identity().translate(x, y);
+		this.model.draw(surface);
+	}
+}
+
+class TintShader extends Shader
+{
+	constructor()
+	{
+		super({
+			vertexFile:   '#/shaders/image.vert.glsl',
+			fragmentFile: '#/shaders/image.frag.glsl',
+		});
+	}
+
+	setColor(color)
+	{
+		this.setColorVector('tintColor', color);
 	}
 }
