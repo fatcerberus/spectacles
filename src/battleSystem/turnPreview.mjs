eb480bcca889d40e7b4e9871981d38d3a9b69f65
Bruce Pascoe <fatcerberus@icloud.com>
2018-03-08 12:01:44 -0500

    Further optimize TurnPreview rendering
    Pre-render the background elements too, to avoid creating a bunch of
    temporary shapes and vertex buffers.

--- src/battleSystem/turnPreview.mjs
+++ src/battleSystem/turnPreview.mjs
@@ -16,8 +16,27 @@ class TurnPreview extends Thread
 
 		this.entries = {};
 		this.fadeness = 1.0;
-		this.font = GetSystemFont();
 		this.lastPrediction = null;
+
+		let font = Font.Default;
+		let outlineColor = Color.Black.fadeTo(0.25);
+		let surface = new Surface(160, 16);
+		let boxColor = Color.Black.fadeTo(0.66);
+		let borderColor = Color.Black.fadeTo(0.75);
+		Prim.drawRectangle(surface, 0, 0, 48, 16, 1.0, borderColor);
+		Prim.drawSolidRectangle(surface, 1, 1, 46, 14, boxColor);
+		font.drawText(surface, 11, 3, "next:", Color.Black);
+		font.drawText(surface, 10, 2, "next:", Color.Silver);
+		Prim.drawRectangle(surface, 48, 0, 112, 16, 1.0, borderColor);
+		Prim.drawSolidRectangle(surface, 49, 1, 110, 14, boxColor);
+		this.transform = new Transform();
+		this.shape = new Shape(ShapeType.TriStrip, surface.toTexture(),
+			new VertexList([
+				{ x: 0,   y: 0,  u: 0.0, v: 1.0 },
+				{ x: 160, y: 0,  u: 1.0, v: 1.0 },
+				{ x: 0,   y: 16, u: 0.0, v: 0.0 },
+				{ x: 160, y: 16, u: 1.0, v: 0.0 },
+			]));
 	}
 
 	dispose()
@@ -89,11 +108,8 @@ class TurnPreview extends Thread
 		let alpha = 255 * (1.0 - this.fadeness);
 		let y = -16 * this.fadeness;
 		Surface.Screen.clipTo(0, y, 160, 16);
-		Rectangle(0, y, 48, 16, CreateColor(0, 0, 0, alpha * 0.75));
-		OutlinedRectangle(0, y, 48, 16, CreateColor(0, 0, 0, alpha * 0.125));
-		drawTextEx(this.font, 24, y + 2, "next:", CreateColor(128, 128, 128, alpha), 1, 'center');
-		Rectangle(48, y, 112, 16, CreateColor(0, 0, 0, alpha * 0.75));
-		OutlinedRectangle(48, y, 112, 16, CreateColor(0, 0, 0, alpha * 0.125));
+		this.transform.identity().translate(0, y);
+		this.shape.draw(Surface.Screen, this.transform);
 		for (const id in this.entries) {
 			let entry = this.entries[id];
 			for (let i = 0; i < entry.turnBoxes.length; ++i) {
@@ -116,6 +132,7 @@ class BattlerIcon
 		Prim.drawRectangle(surface, 0, 0, 16, 16, 1, outlineColor);
 		font.drawText(surface, 5, 3, name[0], Color.Black);
 		font.drawText(surface, 4, 2, name[0], Color.mix(Color.White, color));
+		this.transform = new Transform();
 		this.shape = new Shape(ShapeType.TriStrip, surface.toTexture(),
 			new VertexList([
 				{ x: 0,  y: 0,  u: 0.0, v: 1.0 },
@@ -127,7 +144,7 @@ class BattlerIcon
 
 	drawAt(surface, x, y)
 	{
-		let transform = new Transform().translate(x, y);
-		this.shape.draw(surface, transform);
+		this.transform.identity().translate(x, y);
+		this.shape.draw(surface, this.transform);
 	}
 }
