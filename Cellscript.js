de7708eec5824995cd308198728cb8820a831933
Bruce Pascoe <fatcerberus@icloud.com>
2024-12-18 14:00:57 -0500

    Update title card
    Because, you know, where'd she go? The world may never know!

--- Cellscript.js
+++ Cellscript.js
@@ -9,7 +9,7 @@ describe({
 	apiLevel: 4,
 	name: "Spectacles: Bruce's Story",
 	author: "Where'd She Go? Productions",
-	summary: "Follow Scott Starcross in his quest to stop the Primus from destroying both worlds.",
+	summary: "Follow Scott Starcross in his quest to stop the Primus from, um... what does the Primus do again?",
 	resolution: '320x240',
 	main: 'scripts/main.js',
 	saveID: 'Spectacles Saga',
